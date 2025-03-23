a394f6d370dccb2006df8bc7566a7e6f527f1596
chromody <chromody@gmail.com>
2025-03-22 22:18:13 -0500

    made register user endpoint

--- src/api/api.rs
+++ src/api/api.rs
@@ -9,11 +9,12 @@ use futures::stream::TryStreamExt;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
+use regex::Regex;
+
 // import macro to create response
 use crate::responses::responses::Response; // Now Rust can find it
 use crate::new_response;
-use crate::schema::schema::User;
-use crate::schema::schema::create_user;
+use crate::schema::schema::{UserRegister, UserError};
 use crate::db::db::Database;
 
 
@@ -26,17 +27,186 @@ fn index() -> Result<Json<Response>, Status> {
 }
     */
     
-#[post("/user/register")]
-async fn register(db: &State<Database>) -> Result<Json<Response>, Status> {
-    let collection: Collection<User> = db.client.lock().await.database("rocket-template").collection("users");
+/*
+#[get("/user")]
+async fn get_user() -> Result<Json<Response>, (Status, Json<Response>)> {
+    let user: User;
+    let user_result: Result<User, UserError> = User::new("Chromody".to_string(), "Chromody@chromody.com".to_string(), "Chromody".to_string(), "Chromody1234@".to_string());
+    match user_result {
+        Ok(user_unwrapped) =>{
+            user = user_unwrapped;
+        } //checking for each type of error
+        Err(UserError::InvalidEmail) => {
+            return Err(
+                (
+                    Status::BadRequest,
+                    Json(
+                        new_response!(
+                            json!(
+                                {
+                                    "message": "Invalid email",
+                                    "error": "Invalid email"
+                                }
+                            )
+                        )
+                    )
+                )
+            );
+        }
+        Err(UserError::InvalidPassword) => {
+            return Err(
+                (
+                    Status::BadRequest,
+                    Json(
+                        new_response!(
+                            json!(
+                                {
+                                    "message": "Invalid password",
+                                    "error": "Invalid password"
+                                }
+                            )
+                        )
+                    )
+                )
+            );
+        }
+        Err(UserError::InvalidUsername) => {
+            return Err(
+                (
+                    Status::BadRequest,
+                    Json(
+                        new_response!(
+                            json!(
+                                {
+                                    "message": "Invalid username",
+                                    "error": "Invalid username"
+                                }
+                            )
+                        )
+                    )
+                )
+            );
+        }
+        Err(_) => {
+            return Err(
+                (
+                    Status::InternalServerError,
+                    Json(
+                        new_response!(
+                            json!(
+                                {
+                                    "message": "Internal server error",
+                                    "error": "Internal server error"
+                                }
+                            )
+                        )
+                    )
+                )
+            );
+        }
+    }
+    let response = new_response!(json!(user.to_json()));
+    Ok(Json(response))
+}
+*/
+
+
+#[post("/user/register", data = "<register_data>")]
+async fn register(db: &State<Database>, register_data: Json<UserRegister>) -> Result<Json<Response>, (Status, Json<Response>)> {
+    let collection: Collection<UserRegister> = db.client.lock().await.database("rocket-template").collection("users");
 
     //using macro to create a new response
-    let user = create_user("username".to_string(), "email".to_string(), "user_id".to_string(), "password".to_string());
+    let user: UserRegister;
+    let user_result: Result<UserRegister, UserError> = UserRegister::new(register_data.get_username().to_string(), register_data.get_email().to_string(), register_data.get_password().to_string());
+    match user_result {
+        Ok(user_unwrapped) =>{
+            user = user_unwrapped;
+        } //checking for each type of error
+        Err(UserError::InvalidEmail) => {
+            return Err(
+                (
+                    Status::BadRequest,
+                    Json(
+                        new_response!(
+                            json!(
+                                {
+                                    "error": "Invalid email",
+                                    "email": register_data.get_email()
+                                }
+                            )
+                        )
+                    )
+                )
+            );
+        }
+        Err(UserError::InvalidPassword) => {
+            return Err(
+                (
+                    Status::BadRequest,
+                    Json(
+                        new_response!(
+                            json!(
+                                {
+                                    "error": "Invalid password",
+                                    "password": register_data.get_password()
+                                }
+                            )
+                        )
+                    )
+                )
+            );
+        }
+        Err(UserError::InvalidUsername) => {
+            return Err(
+                (
+                    Status::BadRequest,
+                    Json(
+                        new_response!(
+                            json!(
+                                {
+                                    "error": "Invalid username",
+                                    "username": register_data.get_username()
+                                }
+                            )
+                        )
+                    )
+                )
+            );
+        }
+        Err(_) => {
+            return Err(
+                (
+                    Status::InternalServerError,
+                    Json(
+                        new_response!(
+                            json!(
+                                {
+                                    "error": "Internal server error"
+                                }
+                            )
+                        )
+                    )
+                )
+            );
+        }
+    }
+
     if collection.insert_one(&user).await.is_ok() {
         let response = new_response!(user.to_json());
         Ok(Json(response))
     } else {
-        Err(Status::InternalServerError)
+        Err((
+            Status::InternalServerError,
+            Json(
+                new_response!(
+                    json!(
+                        {
+                            "error": "Internal server error",
+                        }
+                    )
+                )
+            )
+        ))
     }
 }
 
--- src/schema/schema.rs
+++ src/schema/schema.rs
@@ -1,34 +1,73 @@
 use rocket::serde::{Deserialize, Serialize};
 use bcrypt::{hash, DEFAULT_COST};
+use regex::Regex;
+
+pub enum UserError {
+    InvalidEmail,
+    InvalidPassword,
+    InvalidUsername,
+}
 
 #[derive(Deserialize, Serialize)]
-pub struct User {
+pub struct UserRegister {
     username: String,
     email: String,
-    user_id: String,
     password: String,
 }
 
-impl User {
+impl UserRegister {
+    pub fn new(username: String, email: String, password: String) -> Result<Self, UserError> {
+        let EMAIL_REGEX: Regex = Regex::new(r".$").expect("Invalid regex");
+        let PWD_REGEX: Regex = Regex::new(r".$").expect("Invalid regex");
+        let UNAME_REGEX: Regex = Regex::new(r".$").expect("Invalid regex");
+
+        if !EMAIL_REGEX.is_match(&email) {
+            return Err(UserError::InvalidEmail);
+        }
+        if !PWD_REGEX.is_match(&password) {
+            return Err(UserError::InvalidPassword);
+        }
+        if !UNAME_REGEX.is_match(&username) {
+            return Err(UserError::InvalidUsername);
+        }
+
+        let password = hash(password, DEFAULT_COST).unwrap();
+
+        Ok(UserRegister {
+            username: username,
+            email: email,
+            password: password,
+        })
+    }
+    pub fn check_password(&self, password: &str) -> bool {
+        bcrypt::verify(password, &self.password).unwrap()
+    }
+    pub fn get_username(&self) -> &str {
+        &self.username
+    }
+    pub fn get_email(&self) -> &str {
+        &self.email
+    }
+    pub fn get_password(&self) -> &str {
+        &self.password
+    }
+    pub fn set_username(&mut self, username: String) {
+        self.username = username;
+    }
+    pub fn set_email(&mut self, email: String) {
+        self.email = email;
+    }
+    pub fn set_password(&mut self, password: String) {
+        self.password = password;
+    }
+
     pub fn to_json(&self) -> serde_json::Value {
         serde_json::json!({
             "user": {
                 "username": self.username,
                 "email": self.email,
-                "user_id": self.user_id,
                 "password": self.password,
             }
         })
-    }
-}
-
-pub fn create_user(username: String, email: String, user_id: String, password: String) -> User {
-    let password = hash(password, DEFAULT_COST).unwrap();
-
-    User {
-        username: username,
-        email: email,
-        user_id: user_id,
-        password: password,
     }
 }
